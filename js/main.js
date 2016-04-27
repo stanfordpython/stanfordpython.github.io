23754a7999a2220d3c97ba873ae356dab7c2df27
Sam Redmond <sredmond@stanford.edu>
2016-04-27 07:12:22 -0700

    Completely revamps the website to use new styles and organization

--- js/main.js
+++ js/main.js
@@ -26,4 +26,32 @@ $(document).ready(function() {
       }
     }
   });
+
+  $('#navigation a[data-toggle="tab"]').click(function (e) {
+    $(this).tab('show');
+    e.preventDefault();
+  })
+
+  // TODO: better tab switching!
+
+  // Switch to active tab on reload
+  if (window.location.hash !== '') {
+    $('#navigation a[href="' + window.location.hash + '"]').tab('show');
+  }
+
+  // Add hash to URL and push to browser history
+  $('#navigation a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
+     if(history.pushState) {
+          history.pushState(null, null, '#' + $(e.target).attr('href').substr(1));
+     } else {
+          window.location.hash = '#' + $(e.target).attr('href').substr(1);
+     }
+  });
+});
+
+// Switch to the active tab when the browser jumps backwards in state.
+$(window).on('popstate', function() {
+  if (window.location.hash !== '') {
+    $('#navigation a[href="' + window.location.hash + '"]').tab('show');
+  }
 });
