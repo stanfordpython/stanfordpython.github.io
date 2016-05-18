1772ccc1c3d7eaf252c3a6d9196141c92fe8d1b0
Sam Redmond <sredmond@stanford.edu>
2016-05-18 02:55:43 -0700

    Organizes into archived webpages

---
+++ archive/original/js/main.js
@@ -0,0 +1,15 @@
+// by Sherman Leung
+
+// Initialize varibles
+var $window = $(window);
+var $usernameInput = $('.usernameInput'); // Input for username
+var $messages = $('.messages'); // Messages area
+var $inputMessage = $('.inputMessage'); // Input message input box
+
+var $splashPage = $('.splash.page'); // The login page
+var $detailPage = $('.detail.page'); // The chatroom page
+
+$('#learn-more').click(function() {
+  $splashPage.fadeOut();
+  $detailPage.show();
+})
