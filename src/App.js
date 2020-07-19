8f8718c811e3e44f0b581556dc53cfd7c75b0666
cooper-mj <coopermj@stanford.edu>
2020-07-19 16:40:57 -0700

    Added HashRouter - buggy since everything routes to homepage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import { Home } from './Home';
 import { About } from './About';
 import { Contact } from './Contact';
@@ -11,17 +11,15 @@ class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <Router>
-          <NavigationBar />
+        <NavigationBar />
+
+        <HashRouter>
           <Layout>
-            <Switch>
-              <Route exact path="/" component={Home} />
+              <Route path="/" component={Home} />
               <Route path="/about" component={About} />
               <Route path="/contact" component={Contact} />
-              <Route component={NoMatch} />
-            </Switch>
           </Layout>
-        </Router>
+        </HashRouter>
       </React.Fragment>
     );
   }
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -22,8 +22,8 @@ export const NavigationBar = () => (
 			<Navbar.Collapse id="basic-navbar-nav">
 				<Nav className="ml-auto">
 					<Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item>
-					<Nav.Item><Nav.Link href="/about">About</Nav.Link></Nav.Item>
-					<Nav.Item><Nav.Link href="/contact">Contact</Nav.Link></Nav.Item>
+					<Nav.Item><Nav.Link href="/about/">About</Nav.Link></Nav.Item>
+					<Nav.Item><Nav.Link href="/contact/">Contact</Nav.Link></Nav.Item>
 
 				</Nav>
 
