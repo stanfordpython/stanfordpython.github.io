70cd9bcd323394def0a7b3b7e4914f23b0316a3e
cooper-mj <coopermj@stanford.edu>
2020-08-08 12:13:55 -0700

    Markdown rendering through ComponentDidMount and promises

--- src/NoMatch.js
+++ src/NoMatch.js
@@ -1,14 +1,60 @@
 import React, { Component } from 'react';
-import ReactMd from 'react-md-file';
+import ReactMarkdown from 'react-markdown';
+
 
 export class NoMatch extends Component {
 
-	render() {
-	return (
-		<div className="content">
-		<ReactMd fileName={this.props.match.params.slug.concat(".md")} />
-		</div>
-	);
+	// Constructor
+	constructor(props) {
+		super(props);
+		this.state = {
+		  md: '',
+		};
+	  }
+
+	// fetch the md file
+	fetchFile(file) {
+		let request = new Request(file, {
+		headers: new Headers({
+			'Content-Type': 'text/plain'
+		})
+		});
+		return fetch(request).then(res => {
+		if (!res.ok) {
+			throw new Error(res.statusText);
+		}
+
+		return res.text().then(text => {
+			return text;
+		});
+		}).catch(err => {
+		throw new Error('Failed fetching file: ' + err.message);
+		});
+	}
+
+	componentDidMount() {
+		var mdPromise = this.fetchFile(
+			this.props.match.params.slug.concat(".md")
+			);
+		
+		mdPromise.then(result => {
+			// Success!
+			console.log("Result")
+			console.log(result);
+			this.setState({md: result});
+		}, function(value) {
+			// Failure!
+			this.setState({md: "404 Page Not Found"});
+		});
+		
+	}
+
+	render() {			
+
+		return (
+			<div className="content">
+			<ReactMarkdown source={this.state.md} />
+			</div>
+		);
 	}
 }
-
