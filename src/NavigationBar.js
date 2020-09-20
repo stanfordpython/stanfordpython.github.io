b084c7f8ba1b6b1482d16208cc2bca9b9a72eb0c
Parth Sarin <parthsarin@gmail.com>
2020-09-20 14:53:48 -0500

    Toggle navbar on select

--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Nav, Navbar } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
-export const NavigationBar = () => (
-	<Navbar variant="dark" expand="lg">
-		<Navbar.Brand href="/">CS 41</Navbar.Brand>
-		<Navbar.Toggle aria-controls="basic-navbar-nav" />
-		<Navbar.Collapse id="basic-navbar-nav">
-			<Nav className="ml-auto">
-				<RouterNavLink to="/">Home</RouterNavLink>
-				<RouterNavLink to="/lectures">Lectures</RouterNavLink>
-				<RouterNavLink to="/labs">Labs</RouterNavLink>
-				<RouterNavLink to="/assignments">Assignments</RouterNavLink>
-			</Nav>
-		</Navbar.Collapse>
+export const NavigationBar = () => {
+	const [expanded, setExpanded] = useState(false);
 
-	</Navbar>
-)
+	return (
+		<Navbar 
+			onToggle={(e) => { setExpanded(e) }} 
+			expanded={expanded}
+			variant="dark" 
+			expand="lg"
+		>
+			<Navbar.Brand href="/">CS 41</Navbar.Brand>
+			<Navbar.Toggle aria-controls="basic-navbar-nav" />
+			<Navbar.Collapse id="basic-navbar-nav">
+				<Nav className="ml-auto" onSelect={() => { setExpanded(false) }}>
+					<RouterNavLink to="/">Home</RouterNavLink>
+					<RouterNavLink to="/lectures">Lectures</RouterNavLink>
+					<RouterNavLink to="/labs">Labs</RouterNavLink>
+					<RouterNavLink to="/assignments">Assignments</RouterNavLink>
+				</Nav>
+			</Navbar.Collapse>
+
+		</Navbar>
+	);
+}
 
 const RouterNavLink = ({ children, ...props }) => (
   <LinkContainer exact {...props}>
