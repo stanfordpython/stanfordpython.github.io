33b975cee2a261dcee83ad3e2b3daa89654cf29a
Parth Sarin <psarin@stanford.edu>
2020-09-24 12:43:28 -0500

    Make resources local

--- public/archive/win20/js/resources.js
+++ public/archive/win20/js/resources.js
@@ -426,7 +426,7 @@ function create_week_heading_tr(week) {
 
 // Actually fetch all the resources
 //var RESOURCES_DIR = 'http://localhost:8000/res'
-var RESOURCES_DIR = 'https://stanfordpython.com/res'
+var RESOURCES_DIR = './res'
 $.when(
   // Before we can do anything, make sure we have the proper data!
   $.getJSON(RESOURCES_DIR + '/announcements.json', function(data) {
