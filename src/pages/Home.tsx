97c649bccec5320c641b267469ddd173f06a833d
Parth Sarin <psarin@stanford.edu>
2020-09-26 12:42:20 -0500

    Add banner directing people to the archive

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,9 +4,16 @@ import { Schedule } from "../components/Schedule";
 import SiteHeader from "../components/SiteHeader";
 import CourseInfo from "../components/CourseInfo";
 
+import Alert from 'react-bootstrap/Alert';
+
 const Home: FunctionComponent = () => (
     <div className="home">
         <SiteHeader />
+        <Alert variant='info'>
+            Looking for an older version of this site? Check out&nbsp;
+            <Alert.Link href="https://stanfordpython.com/archive">the archive
+            </Alert.Link>.
+        </Alert>
         <CourseInfo />
         <Schedule/>
     </div>
