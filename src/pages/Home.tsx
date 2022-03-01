ed89a3afb77ef4155cf766b746eb427a266df0d7
Parth Sarin <psarin@stanford.edu>
2022-03-01 09:12:34 -0800

    enrollment issues resolved

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,18 +7,15 @@ import { Alert } from "react-bootstrap";
 
 const Home: FunctionComponent = () => (
     <div className="home">
-        <Alert variant="danger" className="mt-4">
+        <Alert variant="success" className="mt-4">
             <Alert.Heading>
                 <h2 className="mt-0">Enrollment Issues</h2>
             </Alert.Heading>
-            <p>
-                We're having some issues with course enrollment through Axess.
-                If you try to enroll, you might encounter an issue where you
-                have to select a discussion section, but none of them are open,
-                preventing you from enrolling in the course. Sorry for the 
-                stress! We are able to accept anyone who wants to enroll in the
-                course and we're working on fixing this. Thanks for your 
-                patience 💜
+            <p className="mb-1">
+                There have been some issues with course enrollment through Axess,
+                but they should be resolved now. If you have any issues, please
+                contact the course staff for a permission number: 
+                <a href="mailto:psarin@stanford.edu,tarabeth@stanford.edu">email Parth and Tara</a>.
             </p>
         </Alert>
         <SiteHeader />
