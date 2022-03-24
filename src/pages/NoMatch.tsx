f76650b462843a103c49eeaff64504aa3d00cd6a
Parth Sarin <psarin@stanford.edu>
2022-03-23 22:50:11 -0500

    add a0

--- src/pages/NoMatch.tsx
+++ src/pages/NoMatch.tsx
@@ -9,29 +9,28 @@ const NoMatch: FunctionComponent<{}> = () => {
 			<p>
 				When Parth decided to explore,<br></br>
 				And browse the Internet;<br></br>
-				He dreamed of servers near and far,<br></br>
-				To which he could connect.<br></br>
+				They dreamed of servers near and far,<br></br>
+				To which they could connect.<br></br>
 				<br></br>
-				"For knowledge of CS", he spoke,<br></br>
+				"For knowledge of CS", they spoke,<br></br>
 				"I've always sought and yearned -<br></br>
 				Now Python, Java, C++,<br></br>
 				are here for me to learn!"<br></br>
 				<br></br>
 				So Parth obtained a URL,<br></br>
-				He typed it in with pride.<br></br>
+				They typed it in with pride.<br></br>
 				But Parth received a 404.<br></br>
 				And Parth's connection died.<br></br>
 				<br></br>
-				"It's hopeless - helpless!" he exclaimed,<br></br>
+				"It's hopeless - helpless!" they exclaimed,<br></br>
 				"Whatever shall I do?"<br></br>
-				He puzzled over what, pray tell,<br></br>
-				Was driving him askew.<br></br>
+				They puzzled over what, pray tell,<br></br>
+				Was driving them askew.<br></br>
 				<br></br>
-				Painstakingly, Parth checked his link,<br></br>
+				Painstakingly, Parth checked their link,<br></br>
 				Abrim with angst and pain.<br></br>
-				He fixed the typo in his link.<br></br>
+				They fixed the typo in their link.<br></br>
 				And all was good again.<br></br>
-
 			</p>
 		</div>
 	);
